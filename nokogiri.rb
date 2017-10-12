fa415b6c60ef7fee62f0db4b1b39aa5d8d254573
Halvard75 <metivier.anto@gmail.com>
2017-10-12 15:23:22 +0200

    1ere sauvegare

---
+++ nokogiri.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+
+doc = Nokogiri::HTML(open('http://annuaire-des-mairies.com/hauts-de-seine.html').read)
+
+puts "### Search for nodes by css"
+urls = doc.search('a.lientxt').map { |tag|
+  case tag.name.downcase
+  when 'a'
+    tag['href']
+  end
+  }
+  puts "http://annuaire-des-mairies.com/hauts-de-seine.html/ + #{urls}\n"
+
+
+
+=begin
+# Fetch and parse HTML document
+doc = Nokogiri::HTML(open('http://annuaire-des-mairies.com/92/sceaux.html'))
+
+puts "### Search for nodes by css"
+doc.css('p:contains("@")').each do |link|
+  puts link.content
+end
+=end
